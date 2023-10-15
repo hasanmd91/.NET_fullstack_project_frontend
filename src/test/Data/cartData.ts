5a3176afd2e65f3656c5efc4c72e27f1d31b5df8
hasanmd91 <hasanmd1991@hotmail.com>
2023-10-15 17:39:23 +0300

    build cart tests

---
+++ src/test/Data/cartData.ts
@@ -0,0 +1,38 @@
+import { CartItem } from '../../types/cart';
+
+const cartItemsData: CartItem[] = [
+  {
+    id: 1,
+    title: 'Another Item',
+    price: 5,
+    quantity: 1,
+    totalPrice: 5,
+    image: 'another-item.jpg',
+  },
+  {
+    id: 2,
+    title: 'Another Item',
+    price: 5,
+    quantity: 1,
+    totalPrice: 5,
+    image: 'another-item.jpg',
+  },
+  {
+    id: 3,
+    title: 'Third Item',
+    price: 5,
+    quantity: 1,
+    totalPrice: 5,
+    image: 'third-item.jpg',
+  },
+  {
+    id: 4,
+    title: 'Special Item',
+    price: 5,
+    quantity: 1,
+    totalPrice: 5,
+    image: 'special-item.jpg',
+  },
+];
+
+export default cartItemsData;
